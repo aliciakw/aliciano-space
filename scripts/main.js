b65d282cbe0cad7ba5b8a18eb4b19a241bd97007
aliciakw <alicia.willett@gmail.com>
2018-07-13 11:40:26 -0400

    Render external handlebars templates using text plugin

--- scripts/main.js
+++ scripts/main.js
@@ -3,24 +3,21 @@ requirejs.config({
     paths: {
         app: '../app',
         data: '../data',
-        //template: '../template',
-        text : '../lib/text'
+        template: '../template'
     }
 });
 
-requirejs(['jquery', 'handlebars'],
-function   ($, Handlebars, data) {
+requirejs(['jquery', 'handlebars', 'text!template/sandbox.html'],
+function   ($, Handlebars, sandboxTemplate) {
     //jQuery, canvas and the app/sub module are all
     //loaded and can be used here now.
     console.log('we have what\'s required');
-    // console.log(text)
     //$('img').hide();
 
 
     // views
-    // var context = { title: 'Black Star', body: '.......'};
-    // var source   = document.getElementById("entry-template").innerHTML;
-    // var template = Handlebars.compile(source);
-    // $('#sandbox').html(template(context));
+    var context = { title: 'Black Star', body: '.......'};
+    var template = Handlebars.compile(sandboxTemplate);
+    $('#sandbox').html(template(context));
 
 });
