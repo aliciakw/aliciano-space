5db7844d37c737ab6668a018ddb40719c1d63ea1
aliciakw <alicia.willett@gmail.com>
2018-07-13 12:53:52 -0400

    Render thumbnail gallery components in loop

--- scripts/main.js
+++ scripts/main.js
@@ -7,19 +7,28 @@ requirejs.config({
     }
 });
 
-requirejs(['jquery', 'handlebars', 'text!template/thumbnail.html'],
-function   ($, Handlebars, thumbnailTemplate) {
-    console.log('we have what\'s required');
-    //$('img').hide();
+requirejs([
+  'jquery',
+  'handlebars',
+  'text!template/thumbnail.html',
+  'text!data/gallery.json'
+],
+($, Handlebars, thumbnailTemplate, rawData) => {
+    var data = JSON.parse(rawData);
+    var interesting = data.interesting;
+    var thumbs = interesting.map((entry) => entry.thumb);
 
     // views
     var s3_bucket = 'https://s3.amazonaws.com/alicia-no-portfolio/';
     var thumbnail_dirname = 'thumbnails/';
-    var filename = 'thumb-grandparents.png';
-    var context = {
-      img_uri: s3_bucket + thumbnail_dirname + filename
-    };
-    var template = Handlebars.compile(thumbnailTemplate);
-    $('#sandbox').html(template(context));
 
+    function renderThumbnail(filename) {
+      var context = {
+        img_uri: s3_bucket + thumbnail_dirname + filename
+      };
+      var template = Handlebars.compile(thumbnailTemplate);
+      $('#thumbnail-gallery-content').append(template(context));
+    }
+
+    thumbs.forEach((thumb) => renderThumbnail(thumb));
 });
