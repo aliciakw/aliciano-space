4851d494b4a1a7d4eae158b31f464ee608a0365a
aliciakw <alicia.willett@gmail.com>
2018-07-13 17:35:09 -0400

    Navigation working, with bugs

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,51 @@ requirejs.config({
 requirejs([
   'jquery',
   'handlebars',
+  'text!data/gallery.json',
+  'text!template/gallery-nav.html',
   'text!template/thumbnail-gallery.html',
-  'text!data/gallery.json'
-],
-($, Handlebars, thumbnailTemplate, rawData) => {
-    var data = JSON.parse(rawData);
 
-    // views
-    var template = Handlebars.compile(thumbnailTemplate);
-    $('#thumbnail-gallery-content').html(template({ paintings: data.interesting }));
+],
+($, Handlebars, rawData, navTemplate, thumbnailTemplate) => {
+    var data = JSON.parse(rawData);
+    var categoryNames = Object.keys(data);
+
+    function getCategoryNavConfig(newCurrentCategory) {
+      return categoryNames.map((categoryName) => ({
+        name: categoryName,
+        selected: categoryName === newCurrentCategory
+      }));
+    }
+    function changeCurrentCategory(categoryName) {
+      currentCategory = categoryName;
+    }
+
+    function render(currentCategory) {
+      var navTemplateCompiled = Handlebars.compile(navTemplate);
+      var thumbnailTemplateCompiled = Handlebars.compile(thumbnailTemplate);
+      $('#gallery-nav-content').html(navTemplateCompiled({
+        config: getCategoryNavConfig(currentCategory)
+      }));
+
+      $('#thumbnail-gallery-content').html(thumbnailTemplateCompiled({
+        title: currentCategory,
+        paintings: data[currentCategory]
+      }));
+      console.log('render is done');
+    }
+
+    // initial render
+    render(categoryNames[0]);
+
+
+    // a lil crude -__-
+    $('li.nav-category').click((event) => {
+      console.log('i see....');
+      var targetId = $(event.target).attr('id');
+      var newCategoryName = targetId.replace('gallery-category-', '');
+      changeCurrentCategory(newCategoryName);
+      //render(newCategoryName);
+    });
+
+
 });
