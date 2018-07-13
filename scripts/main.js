cedebcf05eb5b4da538944a6eb5820b626e4d179
aliciakw <alicia.willett@gmail.com>
2018-07-13 13:34:45 -0400

    move list logic into thumbnail-gallery template

--- scripts/main.js
+++ scripts/main.js
@@ -10,25 +10,13 @@ requirejs.config({
 requirejs([
   'jquery',
   'handlebars',
-  'text!template/thumbnail.html',
+  'text!template/thumbnail-gallery.html',
   'text!data/gallery.json'
 ],
 ($, Handlebars, thumbnailTemplate, rawData) => {
     var data = JSON.parse(rawData);
-    var interesting = data.interesting;
-    var thumbs = interesting.map((entry) => entry.thumb);
 
     // views
-    var s3_bucket = 'https://s3.amazonaws.com/alicia-no-portfolio/';
-    var thumbnail_dirname = 'thumbnails/';
-
-    function renderThumbnail(filename) {
-      var context = {
-        img_uri: s3_bucket + thumbnail_dirname + filename
-      };
-      var template = Handlebars.compile(thumbnailTemplate);
-      $('#thumbnail-gallery-content').append(template(context));
-    }
-
-    thumbs.forEach((thumb) => renderThumbnail(thumb));
+    var template = Handlebars.compile(thumbnailTemplate);
+    $('#thumbnail-gallery-content').html(template({ paintings: data.interesting }));
 });
