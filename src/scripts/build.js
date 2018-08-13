b20c627c9b2f8974fd10c40c4aed91d212d73eac
aliciakw <alicia.willett@gmail.com>
2018-08-13 08:42:43 -0400

    build-public-site in dist folder

---
+++ src/scripts/build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: '.',
+    paths: {
+        jquery: 'lib/jquery',
+        handlebars: 'lib/handlebars',
+        text: 'lib/text'
+    },
+    name: 'main',
+    out: '../../dist/scripts/main.js'
+})
