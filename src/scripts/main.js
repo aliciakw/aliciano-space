c8348b34e70312b3744bd899015d539d190ceaad
aliciakw <alicia.willett@gmail.com>
2018-08-14 18:58:14 -0400

    Bio and startup animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,8 +27,10 @@ requirejs([
       var selectedImageIndex;
       var prevSlug;
       var nextSlug;
-      var prevSlug = selectedImageIndex > 0 ? data[currentCategory][selectedImageIndex - 1].slug : null;
-      var nextSlug = selectedImageIndex < currentCategory.length ? data[currentCategory][selectedImageIndex + 1].slug : null;
+      if (selectedImage) {
+        prevSlug = selectedImageIndex > 0 ? data[currentCategory][selectedImageIndex - 1].slug : null;
+        nextSlug = selectedImageIndex < currentCategory.length ? data[currentCategory][selectedImageIndex + 1].slug : null;
+      }
 
       // render lightbox, if applicable
       if (selectedImage) {
@@ -49,25 +51,31 @@ requirejs([
       // select a collection
       $('li.nav-category').click((event) => {
         var targetId = $(event.target).attr('id');
-        var newCategoryName = targetId.replace('gallery-category-', '');
-        render(newCategoryName);
+        if (targetId) {
+          var newCategoryName = targetId.replace('gallery-category-', '');
+          render(newCategoryName);
+        }
       });
 
       // open lightbox
       $('.thumbnail').click((event) => {
         var targetId = $(event.target).attr('id');
-        var imgSlug = targetId.replace('gallery-img-', '');
-        selectedImage = categoryData.find((image) => image.slug === imgSlug);
-        render(currentCategory, selectedImage);
+        if (targetId) {
+          var imgSlug = targetId.replace('gallery-img-', '');
+          selectedImage = categoryData.find((image) => image.slug === imgSlug);
+          render(currentCategory, selectedImage);
+        }
       });
 
       // change featured image
       $('.left-trigger, .right-trigger').click((event) => {
         event.preventDefault();
         var targetId = $(event.target).attr('id');
-        var imgSlug = targetId.replace('goto-', '');
-        selectedImage = categoryData.find((image) => image.slug === imgSlug);
-        render(currentCategory, selectedImage);
+        if (targetId) {
+          var imgSlug = targetId.replace('goto-', '');
+          selectedImage = categoryData.find((image) => image.slug === imgSlug);
+          render(currentCategory, selectedImage);
+        }
       });
 
       // close lightbox
@@ -94,5 +102,28 @@ requirejs([
     }
 
     // initial render
-    render(categoryNames[0], null);
+    render(null, null);
+
+    // startup animations
+    const sleep = async(ms) => new Promise(resolve => window.setTimeout(resolve, ms));
+    const animateCategories = async (i, max) => {
+      $('.nav-category:nth-child(' + i + ')').css('font-size', '1.5em');
+      if (i > 1) {
+        $('.nav-category:nth-child(' + (i - 1) + ')').css('font-size', '1.0em');
+      }
+      await sleep(200);
+      if (i < max) {
+         return await animateCategories(i + 1, max);
+      }
+      if (i === max) {
+        $('.nav-category:nth-child(' + i + ')').css('font-size', '1em');
+      }
+    }
+    $('document').ready(async () => {
+      $('.vertical-dots-spacer').slideDown();
+      await sleep(300);
+      $('.bio p').fadeIn(600);
+      await animateCategories(1, $('.nav-category').length);
+      $('html, body').animate({ scrollTop: 0 }, "slow");
+    });
 });
